8b90eefd9c1ded3a016751c34e335598d8e1d2a2
Zethembiso Gwala <zethembisogwala@yahoo.com>
2020-04-09 01:31:29 +0200

    map init

---
+++ server.js
@@ -0,0 +1,28 @@
+
+var express = require('express'),
+    app = express();
+
+var path = require('path');
+
+app.use(express.static('www'));
+
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.get('*', function (request, response){
+    response.sendFile( path.resolve() + '/www/index.html')
+}); //does not work //now it does thanks to adding path.resolve()
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), "0.0.0.0", function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,21 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  map: any;
+
+  mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+  
+
   constructor() { }
 
   ngOnInit(): void {
+  	this.mapboxgl.accessToken = 'pk.eyJ1IjoiemV0aGVtYmlzb2d3YWxhIiwiYSI6ImNrOHI5NjZpMjAwd3kzbHBlcDZ6OWtqaDAifQ.nEL_U6fe_KFYZotgCvTYJA'; 
+
+
   	this.initMap()
+
+  	this.setGeolocator()
   }
 
   initMap(){
-  	var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-	mapboxgl.accessToken = 'pk.eyJ1IjoiemV0aGVtYmlzb2d3YWxhIiwiYSI6ImNrOHI5NjZpMjAwd3kzbHBlcDZ6OWtqaDAifQ.nEL_U6fe_KFYZotgCvTYJA';
-	var map = new mapboxgl.Map({
+	this.map = new this.mapboxgl.Map({
   		container: 'map',
-  		style: 'mapbox://styles/mapbox/streets-v11'
+  		style: 'mapbox://styles/zethembisogwala/ck8r9j1p70zoj1io8yv8ybad8',
+  		zoom: 10
 	});
 
+  }
+
+  setGeolocator(){
+  	var geoCtrl = new this.mapboxgl.GeolocateControl({
+    	positionOptions: {
+        	enableHighAccuracy: true
+    	},
+    	trackUserLocation: true
+	});
+
+	this.map.addControl(geoCtrl)
+
+	geoCtrl.trigger()
   }
 
 }
