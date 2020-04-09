c51734532242785917882ffa5f320a855d0088f5
Zethembiso Gwala <zethembisogwala@yahoo.com>
2020-04-09 06:16:21 +0200

    served app from non-dev server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 var express = require('express'),
     app = express();
 
 var path = require('path');
 
-app.use(express.static('www'));
+app.use(express.static('dist/ngRider'));
 
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
@@ -18,7 +17,7 @@ app.all('*', function(req, res, next) {
 // app.get('/blah', routeHandler);
 
 app.get('*', function (request, response){
-    response.sendFile( path.resolve() + '/www/index.html')
+    response.sendFile( path.resolve() + '/dist/ngRider/index.html')
 }); //does not work //now it does thanks to adding path.resolve()
 
 app.set('port', process.env.PORT || 5000);
